c60fb03ee6d94b8cdf6efe74e037bf01cce4d1ad
jakob <jakob.priesner@outlook.de>
2021-05-26 21:02:31 +0200

    new module: akinator

--- Jarvis/modules/akinator.py
+++ Jarvis/modules/akinator.py
@@ -1,6 +1,4 @@
-from akinator import Akinator
-import akinator
-
+from resources.akinator.akinator import Akinator
 PRIORITY = 3
 
 def isValid(text):
@@ -12,9 +10,7 @@ def isValid(text):
 
 def handle(text, core, skills):
     core.say(
-        'Wilkommen zum Rate-spiel! Ich werde dir Fragen stellen, die du wahrheitsgemäß mit wahlweise '
-        '\n"Ja"\n"Nein"\n"wahrscheinlich"\noder "wahrscheinlich nicht" beantworten musst. Wenn du keine Lust mehr auf '
-        'das Spiel hast, sag einfach "stopp" oder "beende das Spiel".')
+        'Wilkommen zum Rate-spiel! Ich werde dir Fragen stellen, die du wahrheitsgemäß mit wahlweise \n"Ja"\n"Nein"\n"wahrscheinlich"\n"wahrscheinlich nicht"\noder "keine Ahnung" beantworten musst. Wenn du keine Lust mehr auf das Spiel hast, sag einfach "stopp" oder "beende das Spiel".')
 
     aki = Akinator()
     question = aki.start_game(language='de')
@@ -25,7 +21,7 @@ def handle(text, core, skills):
         if user_input == "b":
             try:
                 question = aki.back()
-            except akinator.CantGoBackAnyFurther:
+            except Exception:
                 pass
         else:
             question = aki.answer(user_input)
@@ -48,8 +44,28 @@ def assign_user_input(user_input, core):
         return 'no'
     elif 'ja' in user_input or 'richtig' in user_input or 'stimmt' in user_input:
         return 'yes'
+    elif 'wahrscheinlich' in user_input:
+        if 'nicht' in user_input:
+            return 'probably not'
+        else:
+            return 'probably'
+    elif ('keine' in user_input and 'ahnung' in user_input) or ('weiß' in user_input and 'nicht' in user_input):
+        return 'idk'
     else:
-        core.say(
-            'Das habe ich leider nicht verstanden. Versuche es bitte noch einmal mit den Antwortmöglichkeiten '
-            '\n"Ja"\n"Nein"\n"wahrscheinlich"\noder "wahrscheinlich nicht"!')
+        core.say('Das habe ich leider nicht verstanden. Versuche es bitte noch einmal mit den Antwortmöglichkeiten \n"Ja"\n"Nein"\n"wahrscheinlich"\n"wahrscheinlich nicht"\noder "keine Ahnung"!')
         assign_user_input(core.listen(), core)
+
+
+class Core():
+    def __init__(self):
+        pass
+
+    def say(self, text):
+        print(text)
+
+    def listen(self):
+        return input()
+
+
+if __name__ == "__main__":
+    handle("", Core(), None)
