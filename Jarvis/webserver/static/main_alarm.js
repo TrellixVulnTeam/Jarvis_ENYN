a231229c54b620189fde6107b4fac14bb6b51eb5
jakob <jakob.priesner@outlook.de>
2021-09-13 21:12:46 +0200

    first impl of new Websites

--- Jarvis/webserver/static/main_alarm.js
+++ Jarvis/webserver/static/main_alarm.js
@@ -4,14 +4,11 @@ function deleteAlarm(alarm, regular, day) {
     $.get("/api/alarm/delete/"+regular+day+alarm["Zeit"]);
 }
 
-function deleteAllAlarms(regular) {
+function deleteAllAlarms(regular) {}
 
-}
 function getAlarm(regular, alarm_list) {
     var alarms = "";
-
     alarms += "<div id='collapseRegular' class=\"panel-collapse disabled collapse in\" aria-expanded=\"true\">";
-
     for (var day in alarm_list){
         if (day === 0){
             alarms += "<div class=\"panel-body disable\" id=\"regularAlarmSection\">";
@@ -20,25 +17,23 @@ function getAlarm(regular, alarm_list) {
         } else {
             alarms += "<a class=\"list-group-item\" data-toggle=\"collapse\" href=\"#collapseDay" + day + "\">";
             alarms += "<div class=\"list-group-item-heading\" href=\"#\"> " + day + " </div>";
-
             alarms += "<div id=\"collapseDay"+ day +"\" class=\"collapse\">";
             alarms += "<div class=\"list-group\">";
             for (var time in day){
                 alarms += "<a class=\"list-group-item\">";
-            alarms += "<span class=\"mt-1\"> 10:00 </span>";
-            alarms += "<button type=\"button\" class=\"btn btn-danger\" data-toggle=\"modal\" data-target=\"deleteModal("+alarm+")\">löschen</button>";
+                alarms += "<span class=\"mt-1\"> 10:00 </span>";
+                alarms += "<button type=\"button\" class=\"btn btn-danger\" data-toggle=\"modal\" data-target=\"deleteModal("+alarm+")\">löschen</button>";
             }
             alarms += "</a>";
             alarms += "</div>";
             alarms += "</div>";
-            alarms += "</a>"
+            alarms += "</a>";
         }
     }
     alarms += "<div class=\"panel-footer\">";
     alarms += "<button type=\"button\" class=\"btn badge-danger btn-lg\" data-toggle=\"modal\" data-target=\"deleteAllModal('regular')\">alle regulären Wecker löschen</button>";
     alarms += "</div>";
     alarms += "</div>"
-
     $("#regularAlarmSection").html(alarms);
 }
 
@@ -50,40 +45,50 @@ $("#deleteModal").onload = function (alarm, regular, day){
     }
     $("#deleteModalConfirmButton").onclick("deleteAlarm(alarm, regular, day)");
 }
-window.onload = function (){
-    $("#regularAlarmSection").change(function (){
-        $.get("/api/alarm/list/alarms", function (data){
-            var alarms = "";
-            alarms += "<div id='collapseRegular' class=\"panel-collapse disabled collapse in\" aria-expanded=\"true\">";
 
+window.onload = function (){
+    $.get("/api/alarm/list/alarms",
+        function (data){
+        var alarms = "";
+        isPresent = data["regularPresent"];
+        data = data["regular_alarm"];
+        alarms += "<div id='collapseRegular' class=\"panel-collapse disabled collapse in\" aria-expanded=\"true\">";
+        if(isPresent){
             for (var day in data){
-                if (day === 0){
+                if (data[day].length == 0){
                     alarms += "<div class=\"panel-body disable\" id=\"regularAlarmSection\">";
-                    alarms += "<div class=\"list-group-item-heading\" href=\"#\" disabled=\"\"> " + day + " </div>";
+                    alarms += "<div class=\"list-group-item-heading\" href=\"#\" disabled=\"\"> " + day + "</div>";
                     alarms += "</div>";
                 } else {
                     alarms += "<a class=\"list-group-item\" data-toggle=\"collapse\" href=\"#collapseDay" + day + "\">";
-                    alarms += "<div class=\"list-group-item-heading\" href=\"#\"> " + day + " </div>";
-
+                    alarms += "<div class=\"list-group-item-heading\" href=\"#\"> " + day + " " + "<span class=\"badge pull-right badge-secondary\">" + data[day].length + "</span> </div>";
                     alarms += "<div id=\"collapseDay"+ day +"\" class=\"collapse\">";
                     alarms += "<div class=\"list-group\">";
-                    for (var time in day){
+                    for (var time in data[day]){
                         alarms += "<a class=\"list-group-item\">";
-                    alarms += "<span class=\"mt-1\"> 10:00 </span>";
-                    alarms += "<button type=\"button\" class=\"btn btn-danger\" data-toggle=\"modal\" data-target=\"deleteModal("+alarm+")\">löschen</button>";
+                        alarms += "<span class=\"mt-1\">" + data[day][time]["time"]["time_stamp"] + "</span>";
+                        alarms += "<button type=\"button\" class=\"btn pull-right btn-danger btn-sm\" data-toggle=\"modal\" data-target=\"deleteModal("+time+")\">löschen</button>";
                     }
                     alarms += "</a>";
                     alarms += "</div>";
                     alarms += "</div>";
-                    alarms += "</a>"
+                    alarms += "</a>";
                 }
             }
-            alarms += "<div class=\"panel-footer\">";
-            alarms += "<button type=\"button\" class=\"btn badge-danger btn-lg\" data-toggle=\"modal\" data-target=\"deleteAllModal('regular')\">alle regulären Wecker löschen</button>";
-            alarms += "</div>";
-            alarms += "</div>"
-            console.log(alarms)
-            $("#regularAlarmSection").html(alarms);
-        });
-    });
+        }else{
+            alarms += "<div class=\"panel-body disable\">"
+            alarms += "<div class=\"alert alert-info\">";
+            alarms += "<strong>Info!</strong> Keine regulären Wecker eingerichtet.";
+            alarms += "</div></div>";
+        }
+
+        alarms += "<div class=\"panel-footer\">";
+        if(isPresent){
+            alarms += "<button type=\"button\" class=\"btn pull-right badge-danger btn-sm\" data-toggle=\"modal\" data-target=\"deleteAllModal('regular')\">alle regulären Wecker löschen</button>";
+        }
+        alarms += "</div>";
+        alarms += "</div>";
+        $("#regularAlarmSection").html(alarms);
+    }
+    );
 }
--- Jarvis/webserver/static/main_pHUE.js
+++ Jarvis/webserver/static/main_pHUE.js
@@ -1,7 +1,55 @@
 
+function createPHUEBoxes(groups, allLights){
+    let output = "";
+    if (groups != null){
+        console.log("groups != null");
+        for (var gr in groups) {
+            let spezGroup = groups[gr];
+            output += "<div class=\"card border-primary\">";
+            output += "<div class=\"card-header form-inline bg-primary\">";
+            output += "</div>";
+            output += "<div className=\"card-body\">";
+            output += "<ul class=\"list-group\">";
+            let tempLight;
+            for (spezLight in spezGroup["lights"]) {
+                tempLight = allLights[spezGroup["lights"][spezLight]];
+                output += "<li class=\"list-group-item d-flex justify-content-between align-items-center\">";
+                if (tempLight["on"] === true) {
+                    output += "<button class=\"btn btn-outline-success\"><i class=\"bi bi-lightbulb-fill\" onclick=\"\"></i></button>";
+                } else {
+                    output += "<button class=\"btn btn-outline-danger\"><i class=\"bi bi-lightbulb-fill\" onclick=\"\"></i></button>";
+                }
+                output += "<span style=\"margin-left: 50px\"></span>";
+                output += "<span class=\"lampName\">" + tempLight["name"] + "</span>";
+                output += "<span style=\"margin-left: 50px\"></span>";
+                output += "<span class=\"material-icons-outlined\">brightness_2</span>";
+                output += "<input type=\"range\" class=\"form-control-range\" min=\"0\" max=\"254\" value=\"" + tempLight["brightness"] + "\">";
+                output += "<span class=\"material-icons-outlined\">light_mode</span>";
+                output += "</li>";
+                output += "</ul>";
+                output += "</div></div></div></div>";
+            }
+            console.log(groups[gr]["name"] + " done...");
+        }
+    }
+    console.log("creating done...");
+    console.log(output);
+    return output;
+}
 
 window.onload = function (){
-    $("enumeration").change(function (){
-        $.get("/api/phue/list/lights")
-    })
+    var output = "";
+    $.get("/api/phue/list/groups", function (groups){
+        $.get("/api/phue/list/lights?id=true", function (allLights){
+            if (groups != null){
+                for (var gr in groups) {
+                    output += createPHUEBoxes(groups, allLights);
+                }
+            }
+            document.getElementById("lightGroups").innerHTML = output;
+            console.log("creating done...");
+            console.log(output);
+        });
+    });
 }
+
