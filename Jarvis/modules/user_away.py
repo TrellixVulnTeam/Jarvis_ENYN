2dbbc2836015ff06d991287e937a1c696a4bf3ef
jakob <jakob.priesner@outlook.de>
2021-09-27 18:04:39 +0200

    small bugfixes

--- Jarvis/modules/user_away.py
+++ Jarvis/modules/user_away.py
@@ -3,35 +3,14 @@ import random
 
 def handle(text, core, skills):
     user = core.user
-    '''farewells = ['Auf wiedersehen, {}!',
+    farewells = ['Auf wiedersehen, {}!',
                  'Bis bald {}',
                  'Machs gut {}',
-                 'Viel Spaß!']'''
-    farewells = ['Bis bald, wir werden uns wiedersehen...']
+                 'Viel Spaß!']
     farewell = random.choice(farewells)
     if '{}' in farewell:
         farewell = farewell.format(user)
     core.say(farewell)
-
-    # Erst den User aus allen Räumen entfernen...
-    for raum in core.local_storage['rooms'].values():
-        try:
-            if user in raum['users']:
-                raum['users'].remove(user)
-        except KeyError:
-            raum['users'] = []
-            continue
-    for raum in core.rooms.values():
-        if user in raum.users:
-            raum.users.remove(user)
-    # ...Und den Raum aus dem User!
-    try:
-        if not core.local_storage['users'][user]['messenger_id'] == 0:
-            core.local_storage['users'][user]['room'] = 'Telegram'
-        else:
-            core.local_storage['users'][user]['room'] = ''
-    except:
-        pass
 
 
 def isValid(text):
