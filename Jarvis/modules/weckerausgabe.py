5b87ca3e320b4c1c30ca09ccaf52c8191b824130
JakobPri <an5+43B6z>
2021-09-30 11:06:38 +0200

    bugfix

--- Jarvis/modules/weckerausgabe.py
+++ Jarvis/modules/weckerausgabe.py
@@ -1,13 +1,16 @@
 from philips_hue import PhillipsWrapper
 
+
 def handle(text, core, skills):
     ton = text.get('Ton')
     user = text.get('User')
     path = core.path + "/modules/resources/clock_sounds/" + ton
-
-    if core.local_storage['module_storage']['philips_hue']['Bridge-Ip'] != '':
-        pw = PhillipsWrapper(core)
-        pw.wrapper("mach das Licht weiß")
+    try:
+        if core.local_storage['module_storage']['philips_hue']['Bridge-Ip'] != '':
+            pw = PhillipsWrapper(core)
+            pw.wrapper("mach das Licht weiß")
+    except RuntimeError:
+        pass
     try:
         core.play(path=path, next=True)
     except FileNotFoundError:
