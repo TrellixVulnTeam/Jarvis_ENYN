fc87c0be10d35b3681f232aba3844980bf0a17a6
JakobPrie <jakob.priesner@outlook.de>
2021-10-14 23:20:59 +0200

    bugfix

--- Jarvis/webserver/static/main_pHUE.js
+++ Jarvis/webserver/static/main_pHUE.js
@@ -20,7 +20,7 @@ function createPHUEBoxes(groups, allLights) {
             output += "<span style=\"margin-left: 50px\"></span>";
             output += "<span class=\"lampName\">" + tempLight["name"] + "</span>";
             output += "<div style=\"width: 35%\"><span class=\"material-icons-outlined\">brightness_2</span>";
-            output += "<input type=\"range\" class=\"form-control-range\" min=\"0\" max=\"254\" value=\"" + tempLight["brightness"] + "\" onchange=\"changeBrightness('" + tempLight["brightness"] + "', this)\"></div>";
+            output += "<input type=\"range\" class=\"form-control-range\" min=\"0\" max=\"254\" value=\"" + tempLight["brightness"] + "\" onchange=\"changeBrightness('" + tempLight["name"] + "', this)\"></div>";
             output += "<span class=\"material-icons-outlined\">light_mode</span>";
             output += "</li>";
         }
