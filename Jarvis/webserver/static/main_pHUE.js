8ff073c1398a2b3b1ba0aa891abae1b4bfe6d070
JakobPrie <jakob.priesner@outlook.de>
2021-10-14 22:30:45 +0200

    bugfix

--- Jarvis/webserver/static/main_pHUE.js
+++ Jarvis/webserver/static/main_pHUE.js
@@ -20,7 +20,7 @@ function createPHUEBoxes(groups, allLights) {
             output += "<span style=\"margin-left: 50px\"></span>";
             output += "<span class=\"lampName\">" + tempLight["name"] + "</span>";
             output += "<div style=\"width: 35%\"><span class=\"material-icons-outlined\">brightness_2</span>";
-            output += "<input type=\"range\" class=\"form-control-range\" min=\"0\" max=\"254\" value=\"" + tempLight["brightness"] + "\" onchange='changeBrightness("+spezLight+","+this+")'></div>";
+            output += "<input type=\"range\" class=\"form-control-range\" min=\"0\" max=\"254\" value=\"" + tempLight["brightness"] + "\" onchange='changeBrightness("+spezLight+","+this.value()+")'></div>";
             output += "<span class=\"material-icons-outlined\">light_mode</span>";
             output += "</li>";
         }
@@ -31,7 +31,7 @@ function createPHUEBoxes(groups, allLights) {
 }
 
 function changeBrightness(lampName, value){
-    $.get("/api/phue/change/brightness/"+lampName+","+value.value())
+    $.get("/api/phue/change/brightness/"+lampName+","+value)
 }
 
 function changePowerstate(lightname, element) {
