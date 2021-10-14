6935b07b2b0939e5c23532c4a21d75e48b612b9c
JakobPrie <jakob.priesner@outlook.de>
2021-10-14 22:52:42 +0200

    bugfix

--- Jarvis/webserver/static/main_pHUE.js
+++ Jarvis/webserver/static/main_pHUE.js
@@ -31,9 +31,7 @@ function createPHUEBoxes(groups, allLights) {
 }
 
 function changeBrightness(lampName, element){
-    console.log(element.value);
-    console.log(element.val());
-    $.get("/api/phue/change/brightness/"+lampName+"/"+element.val());
+    $.get("/api/phue/change/brightness/"+lampName+"/"+element.value);
 }
 
 function changePowerstate(lightname, element) {
