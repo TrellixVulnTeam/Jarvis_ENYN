53e5d1585180eacdfb5a9c08626dc12f8109f8c7
Jakob <jakob.priesner@outlook.de>
2022-03-19 19:39:19 +0100

    small improvements

--- src/speechassistant/modules/smalltalk.py
+++ src/speechassistant/modules/smalltalk.py
@@ -1,16 +1,13 @@
 import datetime
 import random
 
+from src.speechassistant.core import ModuleWrapper
+from src.speechassistant.resources.module_skills import Skills
+
 SECURE = True
 
-has_dateutil = True  # Wenn `python_dateutil` installiert ist gibt LUNA eine Altersangabe in jahren, monaten und tagen an.
-try:
-    from dateutil import relativedelta
-except ImportError:
-    has_dateutil = False
 
-
-def isValid(text):
+def isValid(text: str) -> bool:
     text = text.lower()
     if ('phobie' in text or 'ängste' in text or 'angst' in text) and (
             'welche' in text or 'was' in text or 'erzähl' in text or 'sag' in text):
@@ -187,7 +184,7 @@ def isValid(text):
         return True
 
 
-def handle(text, core, skills):
+def handle(text: str, core: ModuleWrapper, skills: Skills):
     text = text.lower()
     answer = None
     if 'wie' in text and 'heißt' in text and 'du' in text:
@@ -330,56 +327,10 @@ def handle(text, core, skills):
         elif 'bereit' in text:
             answer = ['Bereit wenn du es bist!']
         elif 'wie' in text and 'alt' in text and 'du' in text:
-            ts = datetime.datetime.now()
-            if not has_dateutil:
-                heute = ts.strftime('%d %b %Y')
-                diff = datetime.datetime.strptime(heute, '%d %b %Y') - datetime.datetime.strptime('6 Feb 2020',
-                                                                                                  '%d %b %Y')
-                daynr = diff.days
-                answer = ['{} Tage seit den ersten Tests.'.format(daynr)]
-            else:
-                geburtsdatum = datetime.datetime.strptime('6 Mai 2020', '%d %b %Y')
-                heute = datetime.datetime.strptime(ts.strftime('%d %b %Y'), '%d %b %Y')
-                diff = relativedelta.relativedelta(heute, geburtsdatum)
-                output_year = ''
-                if diff.years == 1:
-                    output_year = 'Ein Jahr'
-                elif diff.years > 0:
-                    output_year = '{} Jahre'.format(diff.years)
+            birthday = datetime.datetime.strptime('6 Mai 2020', '%d %b %Y')
+            diff = skills.get_time_difference(start_time=birthday)
+            answer = [f'Das System wird seit {diff} entwickelt.']
 
-                output_month = ''
-                if diff.months == 1:
-                    output_month = 'Einen Monat'
-                elif diff.months > 0:
-                    output_month = '{} Monate'.format(diff.months)
-
-                output_days = ''
-                if diff.days == 1:
-                    output_days = 'Einen Tag'
-                elif diff.days > 0:
-                    output_days = '{} Tage'.format(diff.days)
-
-                output = ''
-                if output_year != '':
-                    output = output + output_year
-
-                if output_month != '':
-                    if output != '':
-                        if (output_days == ''):
-                            output = output + ' und '
-                        else:
-                            output = output + ', '
-                    output = output + output_month
-
-                if output_days != '':
-                    if output != '':
-                        output = output + ' und '
-                    output = output + output_days
-
-                if (output == ''):
-                    answer = ['Hast du deine Systemzeit verstellt? Heute sind nicht die ersten Tests.']
-                else:
-                    answer = ['{} seit den ersten Tests.'.format(output)]
         elif 'sicher' in text or 'verschlüsselt' in text or 'verbindung' in text:
             answer = ['Meine internen Verbindungen sind sicher verschlüsselt, bei Telegram weiß ich das nicht so '
                       'genau. Aber generell, bevor du mir irgendwelche Geheimnisse anvertraust: Denk daran, '
