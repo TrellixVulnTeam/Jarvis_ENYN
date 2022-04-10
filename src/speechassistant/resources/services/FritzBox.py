90af38cb61a3ff6ffbd42dba05b02518c6a2b7df
Jakob <jakob.priesner@outlook.de>
2022-04-10 15:35:41 +0200

    first draft of a Fritz Box service

---
+++ src/speechassistant/resources/services/FritzBox.py
@@ -0,0 +1,15 @@
+import logging
+
+from fritzconnection import FritzConnection, FritzMonitor
+
+
+class Connection:
+    def __init__(self, ip='192.168.178.1'):
+        self.conn = FritzConnection(address=ip)
+        self.monitor = FritzMonitor(address=ip)
+        logging.info(f"[INFO] Connected to FritzBox on {ip}")
+        self.monitor.monitor_thread
+
+
+if __name__ == "__main__":
+    c = Connection()
