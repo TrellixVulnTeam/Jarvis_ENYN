729a4f6d8f1cdf1dcdb7b93bbcfb99e6b9f11d07
Jakob <jakob.priesner@outlook.de>
2022-04-24 15:22:42 +0200

    implemented single alarm page

--- src/speechassistant_client/client/src/lib/pages/alarm/alarm.component.ts
+++ src/speechassistant_client/client/src/lib/pages/alarm/alarm.component.ts
@@ -1,10 +1,11 @@
 
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input, OnInit, TemplateRef, ViewChild} from "@angular/core";
 import {Alarm} from "../../data-access/models/alarm";
 import {JsonObject} from "@angular/compiler-cli/ngcc/src/packages/entry_point";
-import {throwMatDialogContentAlreadyAttachedError} from "@angular/material/dialog";
-import {BackendService} from "../../data-access/service/backend.service";
 import {Title} from "@angular/platform-browser";
+import {ActivatedRoute} from "@angular/router";
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {AlarmStore} from "../../data-access/service/alarm.store";
 
 @Component({
   selector: 'alarm',
@@ -15,24 +16,57 @@ export class AlarmComponent implements OnInit{
 
   @Input() alarm: Alarm;
 
-  constructor(private backendService: BackendService, private titleService: Title) {
+  @ViewChild('alarmChangeRepeating') createRepeatModal: TemplateRef<any>;
+  @ViewChild('alarmChangeText') createTextModal: TemplateRef<any>;
+  repeatingModalRef? : BsModalRef;
+  textModalRef?: BsModalRef;
+
+  // public mode: PickerInteractionMode = PickerInteractionMode.DropDown;
+
+  constructor( private alarmStore: AlarmStore,
+               private titleService: Title,
+               private route: ActivatedRoute,
+               private modalService: BsModalService ) {
   }
 
   ngOnInit() {
     this.titleService.setTitle('Wecker');
-    this.alarm = new Alarm({}, true, true, false, true, true, false, false, false, "Guten Morgen!", "standard.wav", true);
+    this.route.params.subscribe(params => {
+      this.alarmStore.getAlarm(params['id']).subscribe(alarm => this.alarm = alarm);
+    });
   }
 
-
-  onDeleteAlarm(id: number): void {
+  onDeleteAlarm( id: number ): void {
+    this.alarmStore.deleteAlarm( id );
   }
 
-  onChangeAlarm(alarm: JsonObject): void {
+  onChangeAlarm( alarm: JsonObject ): void {
 
   }
 
-  onChangeActivationAlarm(id: number, active: boolean): void {
+  onChangeActivationAlarm( id: number, active: boolean ): void {
+    this.alarmStore.updateAlarmActiveStatus( id, active );
+  }
 
+  openRepeatingModal(): void {
+    this.repeatingModalRef = this.modalService.show(this.createRepeatModal);
+  }
+
+  openTextModal(): void {
+    this.textModalRef = this.modalService.show(this.createTextModal);
+  }
+
+  closeRepeatingModal(): void {
+    this.modalService.hide(this.repeatingModalRef?.id);
+  }
+
+  closeTextModal(): void {
+    this.modalService.hide(this.textModalRef?.id);
+  }
+
+  closeAllModals(): void {
+    this.modalService.hide(this.repeatingModalRef?.id);
+    this.modalService.hide(this.textModalRef?.id);
   }
 
   getTimeString(alarm: Alarm): string {
