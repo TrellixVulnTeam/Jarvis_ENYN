677bbdf77c64cdaf79c3d7252e0ea7095ee324ef
Jakob <jakob.priesner@outlook.de>
2022-05-11 12:20:09 +0200

    working on the services

--- src/speechassistant/api/speechassistant/logic/setup/phue.py
+++ src/speechassistant/api/speechassistant/logic/setup/phue.py
@@ -1,5 +1,12 @@
 from flask import Response
+from src.speechassistant.resources.services.light_systems.Phue import PhilipsWrapper
 
+
+service = PhilipsWrapper()
+
+
+def read_device_by_id(_id: int) -> Response:
+    result_set: dict =
 
 def read_phue(data: dict) -> Response:
     pass
