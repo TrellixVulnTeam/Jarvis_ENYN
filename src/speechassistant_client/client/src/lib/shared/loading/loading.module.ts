75b5a07f9da53be192c6c8f5dc509bc99674dbc5
Jakob <jakob.priesner@outlook.de>
2022-05-14 14:08:20 +0200

    added a loading spinner

---
+++ src/speechassistant_client/client/src/lib/shared/loading/loading.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {LoadingComponent} from "./loading.component";
+
+
+@NgModule({
+  declarations: [
+    LoadingComponent
+  ],
+  imports: [],
+  providers: [],
+  bootstrap: [],
+  exports: [LoadingComponent]
+})
+export class LoadingModule {
+}
