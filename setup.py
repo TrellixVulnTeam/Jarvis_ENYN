6f3a20881a9aed8c49bf87d0719c8af9c9eee78c
Jakob <jakob.priesner@outlook.de>
2022-06-24 11:38:16 +0200

    added necessary files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Jarvis",
+    version="0.1",
+    packages=["src"],
+    url="https://github.com/JakobPriesner/Jarvis",
+    license="",
+    author="Jakob Priesner",
+    author_email="jakob.priesner@outlook.de",
+    description="",
+)
