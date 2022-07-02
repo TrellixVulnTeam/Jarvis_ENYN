9bd2e92e32e266dfce16cc52668f99a61f5118f5
Jakob <jakob.priesner@outlook.de>
2022-07-02 11:59:08 +0200

    changed path for Dockerfile

--- src/speechassistant/main.py
+++ src/speechassistant/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from core import Core
 
 if __name__ == "__main__":
-    relPath: str = str(Path(__file__).parent) + "/src/speechassistant/"
+    relPath: str = str(Path(__file__).parent) + "/"
     with open(relPath + "config.json", "r") as config_file:
         config_data: dict = json.load(config_file)
     with open(relPath + "resources/alias/correct_output.json", "r") as correct_output:
