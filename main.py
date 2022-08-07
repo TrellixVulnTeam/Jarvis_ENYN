0bfa0a5af616d9e1d56c367efd50678aa507005c
Jakob <jakob.priesner@outlook.de>
2022-08-07 20:30:52 +0200

    fixed path of config file

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import toml
 
 from src.speechassistant.core import Core
 
+
+def __get_config_path() -> Path:
+    return Path(__file__).parent.joinpath("src").joinpath("speechassistant").joinpath("config.toml").absolute()
+
+
 if __name__ == "__main__":
-    relPath: str = str(Path(__file__).parent)
-    with open(relPath.join("config.toml"), "r") as config_file:
+    with open(__get_config_path(), "r") as config_file:
         config_data: dict[str, any] = toml.load(config_file)
     core = Core.get_instance()
