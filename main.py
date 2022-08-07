8c1cba225a57b52a9e41963cce60bd89f6e98e94
Jakob <jakob.priesner@outlook.de>
2022-08-07 21:42:49 +0200

    bugfix with porcupine creation

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ if __name__ == "__main__":
     with open(__get_config_path(), "r") as config_file:
         config_data: dict[str, any] = toml.load(config_file)
     core = Core.get_instance()
-    core.messenger_thread()
 
     while True:
         try:
