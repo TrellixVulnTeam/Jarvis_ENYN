0fdcc2c5391d635d5a851a306a15cd3dcc86a034
Jakob <jakob.priesner@outlook.de>
2022-08-14 23:18:08 +0200

    removed loading configs in main.py. Up to now it is loaded in core.py

--- speechassistant/main.py
+++ speechassistant/main.py
@@ -1,22 +1,8 @@
 import logging
 import time
 import traceback
-from pathlib import Path
-
-import toml
 
 from src.core import Core
-
-
-def __get_config_path() -> Path:
-    return (
-        Path(__file__)
-        .parent.joinpath("src")
-        .joinpath("speechassistant")
-        .joinpath("config.toml")
-        .absolute()
-    )
-
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -28,8 +14,6 @@ if __name__ == "__main__":
     )
     logging.getLogger().addHandler(logging.StreamHandler())
 
-    with open(__get_config_path(), "r") as config_file:
-        config_data: dict[str, any] = toml.load(config_file)
     core = Core.get_instance()
 
     while True:
