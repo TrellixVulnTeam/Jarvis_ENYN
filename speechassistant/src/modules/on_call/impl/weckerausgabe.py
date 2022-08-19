301f18a6b7c3d624e31d5278bce71ad183703090
JakobPri <an5+43B6z>
2022-08-19 05:55:38 +0200

    refactoring weckerausgabe.py

--- speechassistant/src/modules/on_call/impl/weckerausgabe.py
+++ speechassistant/src/modules/on_call/impl/weckerausgabe.py
@@ -1,32 +1,44 @@
+from src.database.connection import RoutineInterface
 from src.models.user import User
-from src.services import PhilipsWrapper
+from src.modules import Skills, ModuleWrapper
+from src.services.light_systems.phue import PhilipsWrapper
 
 
-def handle(text, core, skills):
+def handle(text: dict, wrapper: ModuleWrapper, skills: Skills):
     ton: str = text.get("Ton")
     user: User = text.get("User")
-    path: str = core.path + "/modules/resources/alarm_sounds/" + ton
-    try:
-        if core.local_storage["module_storage"]["philips_hue"]["Bridge-Ip"] != "":
-            pw: PhilipsWrapper = PhilipsWrapper(core)
-            pw.wrapper("mach das Licht weiß")
-    except RuntimeError:
-        pass
-    try:
-        core.play(path=path, next=True)
-    except FileNotFoundError:
-        core.say("AlarmSchema! AlarmSchema! AlarmSchema! Aufstehen! Klingeling!")
-    if user.first_name == "Unknown":
-        core.say(
-            "Guten Morgen! Ich hoffe du hast gut geschlafen und wünsche dir einen tollen Tag!"
-        )
-    else:
-        core.say(
-            f"Guten Morgen {user.first_name}! Ich hoffe du hast gut geschlafen und wünsche dir einen tollen Tag"
-        )
+    path: str = str(wrapper.path.joinpath("modules", "resources", "alarm", ton))
+    
+    __turn_on_lights(wrapper)
 
-    for routine in core.local_storage["alarm_routines"]:
-        core.start_module(name="start_routine", text=routine, user=core.user)
+    __ring(wrapper, path)
+
+    wrapper.say(__get_output_for_greeting_user(user))
+
+    __start_after_alarm_routines(wrapper)
+
+
+def __start_after_alarm_routines(wrapper):
+    for routine in RoutineInterface().get_all_after_alarm():
+        wrapper.start_module(name="start_routine", text=routine, user=wrapper.user)
+
+
+def __turn_on_lights(wrapper):
+    if wrapper.config["services"]["philips-hue"]["Bridge-IP"]:
+        pw: PhilipsWrapper = PhilipsWrapper(wrapper.config["services"]["philips-hue"]["Bridge-IP"])
+        pw.light_change_color(pw.light_names, "weiß")
+
+
+def __ring(wrapper, path):
+    try:
+        wrapper.play(path=path, as_next=True)
+    except FileNotFoundError:
+        wrapper.say("Alarm! Alarm! Alarm! Aufstehen! Klingeling!")
+
+
+def __get_output_for_greeting_user(user: User) -> str:
+    user_announce: str = "" if not user else f" {user.first_name}"
+    return f"Guten Morgen{user_announce}! Ich hoffe du hast gut geschlafen und wünsche dir einen tollen Tag"
 
 
 def isValid(text):
